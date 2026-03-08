f86110862fc6c5307f79a2bfb4a378c271ff6ca2
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2026-03-08 15:21:26 +1100

    update content labels

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,7 +2,7 @@ resource "proxmox_virtual_environment_storage_nfs" "unraid_media" {
   id      = "unraid-media"
   server  = "192.168.1.132"
   export  = "/mnt/user/Media"
-  content = ["backup"]
+  content = ["images"]
   options = "soft,nofail"
 }
 
@@ -11,7 +11,7 @@ resource "proxmox_virtual_environment_storage_nfs" "unraid_appdata" {
   id      = "unraid-appdata"
   server  = "192.168.1.132"
   export  = "/mnt/user/appdata"
-  content = ["backup"]
+  content = ["images"]
   options = "soft,nofail"
 }
 
@@ -20,6 +20,6 @@ resource "proxmox_virtual_environment_storage_nfs" "unraid_programs" {
   id      = "unraid-programs"
   server  = "192.168.1.132"
   export  = "/mnt/user/Programs"
-  content = ["backup"]
+  content = ["images"]
   options = "soft,nofail"
 }
